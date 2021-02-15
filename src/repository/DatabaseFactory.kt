ad86a77dbecc329d90eb29f929de7afdc87a1b5a
Sahib Singh <your_email@example.com>
2021-02-16 02:29:38 +0530

    API: Modified JDBC Driver Class Name

--- src/repository/DatabaseFactory.kt
+++ src/repository/DatabaseFactory.kt
@@ -23,7 +23,7 @@ object DatabaseFactory {
 
     private fun hikari(): HikariDataSource {
         val config = HikariConfig()
-        config.driverClassName = System.getenv("JDBC_DRIVER") // 1
+        config.driverClassName = "org.postgresql.Driver" // 1
         config.jdbcUrl = System.getenv("JDBC_DATABASE_URL") // 2
         config.maximumPoolSize = 3
         config.isAutoCommit = false
